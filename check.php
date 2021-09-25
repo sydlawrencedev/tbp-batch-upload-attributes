dc638cd44104b8020679ceb5d71ff99547bc5e52
Syd Lawrence <sydlawrence@gmail.com>
2021-09-25 14:51:30 +0100

    adds various php ini tests

---
+++ check.php
@@ -0,0 +1,20 @@
+<?php
+
+require __DIR__ . '/functions.php';
+
+$upload_max_size = ini_get('upload_max_filesize');
+
+$post_max_size = ini_get('post_max_size');
+
+$max_execution_time = ini_get('max_execution_time');
+
+$ini = array(
+    "upload_max_filesize" => "42M",
+    "post_max_size" => "48M",
+    "max_execution_time" => "600"
+);
+
+foreach ($ini as $key => $value) {
+    echo $key.": ".ini_get($key)." - recommended ".$value."<br/>";
+}
+
